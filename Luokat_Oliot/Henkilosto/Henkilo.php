e8d29062f764676d0c8e5fe277d6c1a8077e9999
Mari Leipola <mari.leipola@gmail.com>
2019-08-29 07:59:05 +0300

    Toimiva tietokantayhteys

---
+++ Luokat_Oliot/Henkilosto/Henkilo.php
@@ -0,0 +1,53 @@
+<?php 
+class Henkilo {
+   private $henkilonumero;
+   private $etunimi;
+   private $sukunimi;
+   private $osasto;
+   private $palkka;
+
+   // Tietokantaa varten
+   private $yhteydenhallinta;
+   // void tarkoittaa että funktio ei palauta mitään
+   public function setHenkilonumero($henkilonumero) : void
+   {
+       $this->henkilonumero = $henkilonumero;
+   }
+   public function getHenkilonumero(){
+       return $this->henkilonumero;
+   }
+   public function setEtunimi($etunimi){
+       $this->etunimi = $etunimi;
+   }
+   public function getEtunimi(){
+       return $this->etunimi;
+   }
+   public function setSukunimi($sukunimi){
+       $this->sukunimi = $sukunimi;
+   }
+   public function getSukunimi(){
+       return $this->sukunimi;
+   }
+   public function setOsasto($osasto){
+       $this->osasto = $osasto;
+   }
+   public function getOsasto(){
+       return $this->osasto;
+   }
+   public function setPalkka($palkka){
+       $this->palkka = $palkka;
+   }
+   public function getPalkka(){
+       return $this->palkka;
+   }
+   public function luoYhteysTietokantaan(){
+       //viittaus luokkaan YHteydenHallinta
+       include("YhteydenHallinta.php");
+       // Yhteys tietokantaan
+       $this->yhteydenhallinta = new YhteydenHallinta();
+   }
+   public function haeKaikkiHenkilot(){
+       return $this->yhteydenhallinta->suoritaHakuLause("select * from henkilosto");
+   }
+}
+?>
